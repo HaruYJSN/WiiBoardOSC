1d5e58e9c8b2b016607ebbddcbe45ee906f9ea5e
HaruYJSN <haumume@gmail.com>
2022-06-15 18:13:44 +0900

    プロジェクト ファイルを追加します。

---
+++ WiiBoardTest/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿
+namespace WiiBoardTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.addresstext = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.senderport = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.y_address = new System.Windows.Forms.TextBox();
+            this.x_address = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.d_button = new System.Windows.Forms.Button();
+            this.c_button = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.senderport)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection";
+            // 
+            // addresstext
+            // 
+            this.addresstext.Location = new System.Drawing.Point(80, 13);
+            this.addresstext.Name = "addresstext";
+            this.addresstext.Size = new System.Drawing.Size(100, 23);
+            this.addresstext.TabIndex = 1;
+            this.addresstext.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = ":";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 381);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.senderport);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.progressBar2);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.y_address);
+            this.tabPage1.Controls.Add(this.x_address);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.addresstext);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 353);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "OSC";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(225, 197);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 15);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "label10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(225, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 15);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "label9";
+            // 
+            // senderport
+            // 
+            this.senderport.Location = new System.Drawing.Point(202, 14);
+            this.senderport.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.senderport.Name = "senderport";
+            this.senderport.Size = new System.Drawing.Size(100, 23);
+            this.senderport.TabIndex = 5;
+            this.senderport.Value = new decimal(new int[] {
+            8000,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(202, 71);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(202, 42);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // y_address
+            // 
+            this.y_address.Location = new System.Drawing.Point(80, 71);
+            this.y_address.Name = "y_address";
+            this.y_address.Size = new System.Drawing.Size(100, 23);
+            this.y_address.TabIndex = 1;
+            this.y_address.Text = "/input/Vertical";
+            // 
+            // x_address
+            // 
+            this.x_address.Location = new System.Drawing.Point(80, 42);
+            this.x_address.Name = "x_address";
+            this.x_address.Size = new System.Drawing.Size(100, 23);
+            this.x_address.TabIndex = 1;
+            this.x_address.Text = "/input/Horizontal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "x";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.d_button);
+            this.tabPage2.Controls.Add(this.c_button);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(767, 353);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Bluetooth";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 15);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Y: 0.0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 15);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "X: 0.0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "0.0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Weight(kg)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(183, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // d_button
+            // 
+            this.d_button.Location = new System.Drawing.Point(102, 77);
+            this.d_button.Name = "d_button";
+            this.d_button.Size = new System.Drawing.Size(75, 23);
+            this.d_button.TabIndex = 0;
+            this.d_button.Text = "Disconnect";
+            this.d_button.UseVisualStyleBackColor = true;
+            this.d_button.Click += new System.EventHandler(this.d_button_Click);
+            // 
+            // c_button
+            // 
+            this.c_button.Location = new System.Drawing.Point(102, 48);
+            this.c_button.Name = "c_button";
+            this.c_button.Size = new System.Drawing.Size(75, 23);
+            this.c_button.TabIndex = 0;
+            this.c_button.Text = "Connect";
+            this.c_button.UseVisualStyleBackColor = true;
+            this.c_button.Click += new System.EventHandler(this.c_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "WiiBoard OSC Test";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.senderport)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox addresstext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox y_address;
+        private System.Windows.Forms.TextBox x_address;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown senderport;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button d_button;
+        private System.Windows.Forms.Button c_button;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WiiBoardTest/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiimoteLib;
+using Rug.Osc;
+using System.Net;
+using System.Threading;
+
+namespace WiiBoardTest
+{
+    public partial class Form1 : Form
+    {
+        OscSender oscsend;
+        Wiimote wm = new Wiimote();
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        // OSC送信のセクチョン
+        private void osctrans(String sendsaki, int port, bool flag, bool shokai)
+        {
+            while (flag)
+            {
+                // This is the ip address we are going to send to
+                IPAddress address = IPAddress.Parse(sendsaki);
+                label9.Text = "address: " + sendsaki + "\rPort: " + port.ToString();
+                // Create a new sender instance
+                using (OscSender sender = new OscSender(address, port))
+                {
+                    //if (shokai)
+                    //{
+                        // Connect the sender socket  
+                        sender.Connect();
+                    //    shokai = false;
+                    //}
+                    if (wm.WiimoteState.BalanceBoardState.CenterOfGravity.X / 20 > 0.2 || wm.WiimoteState.BalanceBoardState.CenterOfGravity.X / 20 < -0.2 )
+                    {
+                        // Send a new message
+                        sender.Send(new OscMessage(x_address.Text, wm.WiimoteState.BalanceBoardState.CenterOfGravity.X / 20));
+                    }
+                    else
+                    {
+                        sender.Send(new OscMessage(x_address.Text, 0.0));
+                    }
+                    if (wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y / 12 > 0.2 || wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y / 12 < -0.2)
+                    {
+                        sender.Send(new OscMessage(y_address.Text, wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y / -12));
+                    }
+                    else
+                    {
+                        sender.Send(new OscMessage(y_address.Text, 0.0));
+                    }
+                    label10.Text ="X: " + wm.WiimoteState.BalanceBoardState.CenterOfGravity.X / 20 +"\rY: " + wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y / 12;
+                    
+                }
+                //Thread.Sleep(10);
+                System.Windows.Forms.Application.DoEvents();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            osctrans(addresstext.Text, (int)senderport.Value, true, true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            osctrans(addresstext.Text, (int)senderport.Value, false, false);
+        }
+
+
+        // Wiimoteのセクチョン
+
+        private void c_button_Click(object sender, EventArgs e)
+        {
+            this.wm.Connect();
+            this.wm.WiimoteChanged += wm_WiimoteChanged;
+        }
+
+        private void d_button_Click(object sender, EventArgs e)
+        {
+            this.wm.Disconnect();
+        }
+
+        void wm_WiimoteChanged(object sender, WiimoteChangedEventArgs args)
+        {
+            WiimoteState ws = args.WiimoteState;
+            this.DrawForms(ws);
+
+            this.label6.Text = ws.BalanceBoardState.WeightKg.ToString();
+            this.label7.Text = "X: " + ws.BalanceBoardState.CenterOfGravity.X;
+            this.label8.Text = "Y: " + ws.BalanceBoardState.CenterOfGravity.Y;
+        }
+
+        public void DrawForms(WiimoteState ws)
+        {
+            Graphics g = this.pictureBox1.CreateGraphics();
+            g.Clear(Color.Black);
+
+            // x,y(描画用)の計算
+            float x = (wm.WiimoteState.BalanceBoardState.CenterOfGravity.X + 20.0f) * 10;
+            float y = (wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y + 12.0f) * 10;
+
+            g.FillEllipse(Brushes.Red, x, y, 10, 10);
+            g.Dispose();
+        }
+    }
+}
